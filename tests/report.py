970952a3cd47442a9817f3e477a20780997c0bb3
James Putnam <putnamjm.design@gmail.com>
2023-02-08 10:39:49 -0500

    #62 - tests: add line numbers to report

--- tests/report.py
+++ tests/report.py
@@ -13,6 +13,7 @@ date = datetime.now().strftime('%m/%d/%Y %H:%M:%S')
 print(f'Test Report: {date:<10}')
 print('----------------------')
 
+line_no = 0
 for test in test_results:
     global totals
 
@@ -26,12 +27,16 @@ for test in test_results:
             if len(fields) == 1:
                 pass
             else:
-                if len(fields) == 2 and fields[0].find("panicked") == -1:
-                    print(f'{form:<30} aborted')
-                else:
-                    pass
+                if len(fields) == 2:
+                    if fields[0].find("panicked") != -1:
+                        pass
+                    elif fields[0].find("exception") != -1:
+                        pass
+                    else:
+                        line_no += 1
+                        print(f'{line_no:>3} {form:<30} aborted')
         else:
-            print(f'{form:<30} {fields[1]:<15} {fields[2]:<15} {fields[3]:<15}')
-
+            line_no += 1
+            print(f'{line_no:>3} {form:<30} {fields[1]:<15} {fields[2]:<15} {fields[3]:<15}')
 print('----------------------')
 print(f'{totals[0]} {totals[1]:<26} total: {totals[2]:<8} failed: {totals[3]:<8} aborted: {totals[4]:<8}')    
